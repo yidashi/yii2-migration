305ee8acfcabab692054ff0913a6b03a2a608e5a
yidashi <liujuntaor@qq.com>
2016-11-10 10:01:17 +0800

    删除不必要的依赖

--- static/migration.js
+++ static/migration.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 	$(".select-all").on("click",function() {
-		var box = $(this).parents(".box").first();
+		var box = $(this).closest('.form-group')
 		
-		//@hass-todo 这个有问题。。在文档加载完成后，需要用js坚持模块内是否所有都select然后。设置selectall
 		var selectall = box.data("selectall");
 	
 		if(selectall == true)
--- views/default/index.php
+++ views/default/index.php
@@ -1,5 +1,5 @@
 <?php
-use backend\widgets\ActiveForm;
+use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 use migration\assets\MigrationAsset;
 use migration\models\MigrationUtility;
@@ -21,11 +21,11 @@ $this->title = Yii::t('backend', '迁移');
 	</div>
 </div>
 
-<?= $form->boxField($model, "tableSchemas")->checkboxList(MigrationUtility::getTableNames())->header("迁移表结构")->hint(Html::a("全选",'javascript:void(0)',['class'=>"select-all"]))?>
-<?= $form->boxField($model, "tableDatas")->checkboxList(MigrationUtility::getTableNames())->header("迁移表数据")->hint(Html::a("全选",'javascript:void(0)',['class'=>"select-all"]))?>
+<?= $form->field($model, "tableSchemas")->checkboxList(MigrationUtility::getTableNames())->label("迁移表结构")->hint(Html::a("全选",'javascript:void(0)',['class'=>"select-all"]))?>
+<?= $form->field($model, "tableDatas")->checkboxList(MigrationUtility::getTableNames())->label("迁移表数据")->hint(Html::a("全选",'javascript:void(0)',['class'=>"select-all"]))?>
 
 <div class="form-group">
-     <?= Html::submitButton('迁移', ['class' => 'btn bg-maroon btn-flat btn-block ', 'name' => 'button-submit', 'id' => 'button-submit'])?>
+     <?= Html::submitButton('迁移', ['class' => 'btn btn-primary btn-block ', 'name' => 'button-submit', 'id' => 'button-submit'])?>
 </div>
 <?php ActiveForm::end()?>
 
